c656d40a0066116190235b365e84c116545b02a2
Lucho De Marco <luchodemarco13@gmail.com>
2025-06-24 16:19:38 -0300

    cambio de botones cuando esta logueado y cuando no

--- mi-proyecto/src/components/General/NavBarComponent.jsx
+++ mi-proyecto/src/components/General/NavBarComponent.jsx
@@ -1,22 +1,50 @@
-// NavBarComponent.jsx
-import { Link } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 import '../../assets/styles/NavBarComponent.css';
+import iconoPagina from '../../assets/images/iconoPagina.svg';
 
 const NavBarComponent = () => {
-  return (
-    <nav className="navbar">
-      <Link to="/mazos">Mis Mazos</Link> 
-      <Link to="/jugar-partida">Jugar partida</Link>
-      <Link to="/login">Iniciar sesión</Link>
-      <Link to="/register">Registrarse</Link>
-      <button onClick={cerrarSesion}>Cerrar sesión</button>
-    </nav>
-  );
-};
+  const { loading, isAuthenticated, user, logout } = useAuth();
+  const navigate = useNavigate();
 
-const cerrarSesion = () => {
-  localStorage.removeItem('token'); 
-  window.location.href = '/';
+  if (loading) return null;
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login', { replace: true });
+  };
+
+  return (
+    <>
+      {/* Header blanco con logo y botones de login/registro */}
+      <header className="header">
+      <div className="logo-container">
+        <img src={iconoPagina} alt="Pokepage" className="logo" />
+        <span className="logo-text">Pokepage</span>
+      </div>
+
+      {isAuthenticated ? (
+        <div className="header__user">
+          <button onClick={() => navigate('/mazos')}>Mis mazos</button>
+          <button onClick={() => navigate('/editar-usuario')}>Editar usuario</button>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div className="header__buttons">
+          <button onClick={() => navigate('/register')}>Registro de usuario</button>
+          <button onClick={() => navigate('/login')}>Login</button>
+        </div>
+      )}
+    </header>
+
+    {isAuthenticated && (
+    <section className="saludo-area">
+      <span className="navbar__greeting">Hola {user?.usuario}!!</span>
+    </section>
+  )}
+
+    </>
+  );
 };
 
 export default NavBarComponent;
